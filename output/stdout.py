716a91c4c69d477473761ce99cfd412449fc787e
Adam Jacobson <adam.jacobson@invincea.com>
2018-02-27 15:06:29 -0500

    add octal and hex for numbers to stdout

--- output/stdout.py
+++ output/stdout.py
@@ -19,9 +19,19 @@ class Stdout(object):
         self.options = Options()
         self.options.add_option('separator', 'newline', 'Separator between elements', ['newline', 'comma', 'space',
                                                                                        'tab', 'os_newline'])
+        self.options.add_option('number_format', 'decimal', 'Format for numbers', ['decimal', 'hexadecimal', 'octal'])
 
     def output(self, output_list):
         separator = Stdout._SEPARATORS[self.options['separator']]
-        line = separator.join([str(item) for item in output_list])
+        line = separator.join([self.convert_item(item) for item in output_list])
         sys.stdout.write(line)
         sys.stdout.write(os.linesep)
+
+    def convert_item(self, item):
+        if type(item) is int:
+            if self.options['number_format'] == 'hexdecimal':
+                item = hex(item)
+            elif self.options['number_format'] == 'octal':
+                item = oct(item)
+
+        return str(item)
