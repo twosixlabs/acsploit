28c955d6fa995441a88c5c46ea08fc2761f3d8c5
Sean Al-Gattas <sean.al-gattas@invincealabs.com>
2018-02-27 09:11:14 -0500

    added TODO

--- output/stdout.py
+++ output/stdout.py
@@ -5,6 +5,7 @@ import os
 class Stdout(object):
 
     OUTPUT_NAME = "stdout" #exploits can use this internally to whitelist/blacklist supported output formats
+    #TODO - have an option for decimal, hex, etc
 
     def output(self, output_list):
         for element in output_list:
