6ec3cc532671d8e7d1057121410d562efda53e5a
Adam Jacobson <adam.jacobson@invincea.com>
2018-02-27 15:14:04 -0500

    PEP8 compliance for input tests

--- input/test_strings.py
+++ input/test_strings.py
@@ -1,33 +1,39 @@
-import pytest
 from strings import StringGenerator
 
+
 def test_constructor():
-    stringGen=StringGenerator()
-    assert stringGen.get_min_value() <= stringGen.get_max_value()
+    sg = StringGenerator()
+    assert sg.get_min_value() <= sg.get_max_value()
+
 
 def test_get_random():
-    stringGen=StringGenerator()
-    assert stringGen.is_valid(stringGen.get_random()) # generate a random character and see if it is valid
+    sg = StringGenerator()
+    assert sg.is_valid(sg.get_random())  # generate a random character and see if it is valid
+
 
 def test_get_list_of_values():
-    stringGen=StringGenerator()
-    for string in stringGen.get_list_of_values(10): # if we change the defaults, we may want to test more than 10
-        assert stringGen.is_valid(string)
+    sg = StringGenerator()
+    for string in sg.get_list_of_values(10):  # if we change the defaults, we may want to test more than 10
+        assert sg.is_valid(string)
 
-def test_get_greater_than(): # under the assumption that min and max are different, should we change this?
-    stringGen=StringGenerator()
-    assert stringGen.get_greater_than(stringGen.get_min_value()) > stringGen.get_min_value()
 
-def test_get_less_than():   # under the assumption that min and max are different, should we change this?
-    stringGen=StringGenerator()
-    assert stringGen.get_less_than(stringGen.get_max_value()) < stringGen.get_max_value()
+def test_get_greater_than():  # under the assumption that min and max are different, should we change this?
+    sg = StringGenerator()
+    assert sg.get_greater_than(sg.get_min_value()) > sg.get_min_value()
+
+
+def test_get_less_than():  # under the assumption that min and max are different, should we change this?
+    sg = StringGenerator()
+    assert sg.get_less_than(sg.get_max_value()) < sg.get_max_value()
+
 
 def test_default_min_value():
-    stringGen=StringGenerator()
-    assert stringGen.get_min_value()==chr(int(0x61))
+    sg = StringGenerator()
+    assert sg.get_min_value() == chr(int(0x61))
+
 
 def test_default_max_value():
-    stringGen=StringGenerator()
-    assert stringGen.get_max_value()=="zzzzzzzzzz"
+    sg = StringGenerator()
+    assert sg.get_max_value() == "zzzzzzzzzz"
 
 
