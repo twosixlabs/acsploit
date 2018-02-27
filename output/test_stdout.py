929015c4c9bff55eddbe2e76a5d9ac224e79e2de
Adam Jacobson <adam.jacobson@invincea.com>
2018-02-27 16:47:13 -0500

    improve stdout unit tests
    Devolve conversion tests onto convert_item(), name all tests more clearly, and add one more test

--- output/test_stdout.py
+++ output/test_stdout.py
@@ -4,14 +4,14 @@ from mock import patch
 from stdout import Stdout
 
 
-def test_defaults():
+def test_output_defaults():
     with patch.object(sys, 'stdout') as mock_stdout:
         s = Stdout()
         s.output([7, 'hello', 'c', [1, 2, 3]])
         mock_stdout.write.assert_called_once_with('7\nhello\nc\n[1, 2, 3]' + os.linesep)
 
 
-def test_separator():
+def test_output_separator():
     with patch.object(sys, 'stdout') as mock_stdout:
         s = Stdout()
         s.options.set_value('separator', 'comma')
@@ -19,25 +19,28 @@ def test_separator():
         mock_stdout.write.assert_called_once_with('7,hello,c,[1, 2, 3]' + os.linesep)
 
 
-def test_ints_decimal():
-    with patch.object(sys, 'stdout') as mock_stdout:
-        s = Stdout()
-        s.options.set_value('number_format', 'decimal')
-        s.output([7, 8, 15, 20])
-        mock_stdout.write.assert_called_once_with('7\n8\n15\n20' + os.linesep)
+def test_convert_item_ints_decimal():
+    s = Stdout()
+    s.options.set_value('number_format', 'decimal')
+    for item in [7, 8, 15, 20]:
+        assert s.convert_item(item) == str(item)
 
 
-def test_ints_hexadecimal():
-    with patch.object(sys, 'stdout') as mock_stdout:
-        s = Stdout()
-        s.options.set_value('number_format', 'hexadecimal')
-        s.output([7, 8, 15, 20])
-        mock_stdout.write.assert_called_once_with('0x7\n0x8\n0xf\n0x14' + os.linesep)
+def test_convert_item_ints_hexadecimal():
+    s = Stdout()
+    s.options.set_value('number_format', 'hexadecimal')
+    for item in [7, 8, 15, 20]:
+        assert s.convert_item(item) == hex(item)
 
 
-def test_ints_octal():
-    with patch.object(sys, 'stdout') as mock_stdout:
-        s = Stdout()
-        s.options.set_value('number_format', 'octal')
-        s.output([7, 8, 15, 20])
-        mock_stdout.write.assert_called_once_with('07\n010\n017\n024' + os.linesep)
+def test_convert_item_ints_octal():
+    s = Stdout()
+    s.options.set_value('number_format', 'octal')
+    for item in [7, 8, 15, 20]:
+        assert s.convert_item(item) == oct(item)
+
+
+def test_convert_item_non_int():
+    s = Stdout()
+    for item in ['a', [1, 2, 3]]:
+        assert s.convert_item(item) == str(item)
