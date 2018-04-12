b81bd960996077d692914631799558df57f64c43
Nathan Hauke <nathan.hauke@invincealabs.com>
2018-04-12 15:29:46 -0400

    make max, min private

--- input/numbers.py
+++ input/numbers.py
@@ -12,45 +12,45 @@ class IntGenerator(object):
         self.options.add_option('min_value', 0, 'Minimum integer allowed')
         self.options.add_option('max_value', 255, 'Maximum integer allowed')
 
-        self.min = 0
-        self.max = 255
+        self._min = 0
+        self._max = 255
 
     # sets max, min based on _options; called before generator is used with an exploit
     def prepare(self):
-        self.min = self.options['min_value']
-        self.max = self.options['max_value']
+        self._min = self.options['min_value']
+        self._max = self.options['max_value']
 
     def get_less_than(self, value):
         # return closest int less than value
         # this can be simplified if we know value is an int
-        if value > self.max:
-            return self.max
-        if value <= self.min:
+        if value > self._max:
+            return self._max
+        if value <= self._min:
             raise ValueError('No valid values less than {}'.format(value))
         return int(math.ceil(value) - 1)
 
     def get_greater_than(self, value):
         # return closest int greater than value
         # this can be simplified if we know value is an int
-        if value < self.min:
-            return self.min
-        if value >= self.max:
+        if value < self._min:
+            return self._min
+        if value >= self._max:
             raise ValueError('No valid values greater than {}'.format(value))
         return int(math.floor(value) + 1)
 
     def get_max_value(self):
-        return self.max
+        return self._max
 
     def get_min_value(self):
-        return self.min
+        return self._min
 
     def get_random(self):
-        return random.randint(self.min, self.max)
+        return random.randint(self._min, self._max)
 
     def is_valid(self, value):  # Checks if a candidate value is valid, can be made stronger in the future
-        return self.min <= value <= self.max and int(value) == value
+        return self._min <= value <= self._max and int(value) == value
 
     def get_list_of_values(self, num_values):  # returns a list of valid numbers starting from min_value
-        if self.min + num_values - 1 > self.max:
+        if self._min + num_values - 1 > self._max:
             raise ValueError('Fewer than {} unique values'.format(num_values))
-        return [self.min + i for i in range(num_values)]
+        return [self._min + i for i in range(num_values)]
